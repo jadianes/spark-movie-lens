72715d14ecfe0a90a058b7a36e294fe7773dee1c
cluster <jadianes@gmail.com>
2015-07-05 13:24:44 +0100

    Working towards adding ratings

--- engine.py
+++ engine.py
@@ -7,21 +7,38 @@ logger = logging.getLogger(__name__)
 
 
 def get_counts_and_averages(ID_and_ratings_tuple):
-    """
-    Given a tuple (movieID, ratings_iterable) returns (movieID, (ratings_count, ratings_avg))
+    """Given a tuple (movieID, ratings_iterable) 
+    returns (movieID, (ratings_count, ratings_avg))
     """
     nratings = len(ID_and_ratings_tuple[1])
     return ID_and_ratings_tuple[0], (nratings, float(sum(x for x in ID_and_ratings_tuple[1]))/nratings)
 
 
 class RecommendationEngine:
-    """
-    A movie recommendation engine
+    """A movie recommendation engine
     """
 
-    def __predict_ratings(self, user_and_movie_RDD):
+    def __count_and_average_ratings(self):
+        """Updates the movies ratings counts from 
+        the current data self.ratings_RDD
         """
-        Gets predictions for a given (userID, movieID) formatted RDD
+        logger.info("Counting movie ratings...")
+        movie_ID_with_ratings_RDD = self.ratings_RDD.map(lambda x: (x[1], x[2])).groupByKey()
+        movie_ID_with_avg_ratings_RDD = movie_ID_with_ratings_RDD.map(get_counts_and_averages)
+        self.movies_rating_counts_RDD = movie_ID_with_avg_ratings_RDD.map(lambda x: (x[0], x[1][0]))
+
+
+    def __train_model(self):
+        """Train the ALS model with the current dataset
+        """
+        logger.info("Training the ALS model...")
+        self.model = ALS.train(self.ratings_RDD, self.rank, seed=self.seed,
+                               iterations=self.iterations, lambda_=self.regularization_parameter)
+        logger.info("ALS model built!")
+
+
+    def __predict_ratings(self, user_and_movie_RDD):
+        """Gets predictions for a given (userID, movieID) formatted RDD
         Returns: an RDD with format (movieTitle, movieRating, numRatings)
         """
         predicted_RDD = self.model.predictAll(user_and_movie_RDD)
@@ -33,17 +50,22 @@ class RecommendationEngine:
         
         return predicted_rating_title_and_count_RDD
     
-    def add_ratings(self, user_ratings):
+    def add_ratings(self, ratings):
+        """Add additional movie ratings in the format (user_id, movie_id, rating)
         """
-        Add additional movie ratings in the format (user_id, movie_id, rating)
-        """
-        pass
-
+        # Convert ratings to an RDD
+        new_ratings_RDD = self.sc.parallelize(user_ratings)
+        # Add new ratings to the existing ones
+        self.ratings_RDD = self.ratings_RDD.union(new_ratings_RDD)
+        # Re-compute movie ratings count
+        self.__count_and_average_ratings()
+        # Re-train the ALS model with the new ratings
+        self.__train_model()
+        
+        return ratings
 
     def get_ratings_for_movie_ids(self, user_id, movie_ids):
-        """
-        Given a user_id and a list of movie_ids, predict ratings
-        for them 
+        """Given a user_id and a list of movie_ids, predict ratings for them 
         """
         requested_movies_RDD = self.sc.parallelize(movie_ids).map(lambda x: (user_id, x))
         # Get predicted ratings
@@ -52,8 +74,7 @@ class RecommendationEngine:
         return ratings
     
     def get_top_ratings(self, user_id, movies_count):
-        """
-        Recommends up to movies_count top unrated movies to user_id
+        """Recommends up to movies_count top unrated movies to user_id
         """
         # Get pairs of (userID, movieID) for user_id unrated movies
         user_unrated_movies_RDD = self.movies_RDD.filter(lambda rating: not rating[1]==user_id).map(lambda x: (user_id, x[0]))
@@ -63,8 +84,7 @@ class RecommendationEngine:
         return ratings
 
     def __init__(self, sc, dataset_path):
-        """
-        Init the recommendation engine given a Spark context
+        """Init the recommendation engine given a Spark context
         """
 
         logger.info("Starting up the Recommendation Engine: ")
@@ -74,31 +94,24 @@ class RecommendationEngine:
         # Load ratings data for later use
         logger.info("Loading Ratings data...")
         ratings_file_path = os.path.join(dataset_path, 'ratings.csv')
-        ratings_raw_RDD = sc.textFile(ratings_file_path)
+        ratings_raw_RDD = self.sc.textFile(ratings_file_path)
         ratings_raw_data_header = ratings_raw_RDD.take(1)[0]
         self.ratings_RDD = ratings_raw_RDD.filter(lambda line: line!=ratings_raw_data_header)\
             .map(lambda line: line.split(",")).map(lambda tokens: (int(tokens[0]),int(tokens[1]),float(tokens[2]))).cache()
         # Load movies data for later use
         logger.info("Loading Movies data...")
         movies_file_path = os.path.join(dataset_path, 'movies.csv')
-        movies_raw_RDD = sc.textFile(movies_file_path)
+        movies_raw_RDD = self.sc.textFile(movies_file_path)
         movies_raw_data_header = movies_raw_RDD.take(1)[0]
         self.movies_RDD = movies_raw_RDD.filter(lambda line: line!=movies_raw_data_header)\
             .map(lambda line: line.split(",")).map(lambda tokens: (int(tokens[0]),tokens[1],tokens[2])).cache()
         self.movies_titles_RDD = self.movies_RDD.map(lambda x: (int(x[0]),x[1])).cache()
         # Pre-calculate movies ratings counts
-        logger.info("Counting movie ratings...")
-        movie_ID_with_ratings_RDD = self.ratings_RDD.map(lambda x: (x[1], x[2])).groupByKey()
-        movie_ID_with_avg_ratings_RDD = movie_ID_with_ratings_RDD.map(get_counts_and_averages)
-        self.movies_rating_counts_RDD = movie_ID_with_avg_ratings_RDD.map(lambda x: (x[0], x[1][0]))        
+        self.__count_and_average_ratings()
 
         # Train the model
-        logger.info("Training the ALS model...")
-        best_rank = 8
-        seed = 5L
-        iterations = 10
-        regularization_parameter = 0.1
-        self.model = ALS.train(self.ratings_RDD, best_rank, seed=seed, 
-                               iterations=iterations, lambda_=regularization_parameter)
-        logger.info("ALS model built!")
- 
+        self.rank = 8
+        self.seed = 5L
+        self.iterations = 10
+        self.regularization_parameter = 0.1
+        self.__train_model() 
