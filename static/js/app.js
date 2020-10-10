f48dba4dcd9aa3ca81a3ca358124152329485e2b
Avinash Yadav <itm2016004@iiita.ac.in>
2020-10-10 19:54:37 +0530

    Added Static files and flask templates

---
+++ static/js/app.js
@@ -0,0 +1,45 @@
+(function($, document, window){
+	
+	$(document).ready(function(){
+
+		// Cloning main navigation for mobile menu
+		$(".mobile-navigation").append($(".main-navigation .menu").clone());
+
+		// Mobile menu toggle 
+		$(".menu-toggle").click(function(){
+			$(".mobile-navigation").slideToggle();
+		});
+		$(".search-form button").click(function(){
+			$(this).toggleClass("active");
+			var $parent = $(this).parent(".search-form");
+
+			$parent.find("input").toggleClass("active").focus();
+		});
+
+
+		$(".slider").flexslider({
+			controlNav: false,
+			prevText:'<i class="fa fa-chevron-left"></i>',
+			nextText:'<i class="fa fa-chevron-right"></i>',
+		});
+		if( $(".map").length ) {
+			$('.map').gmap3({
+				map: {
+					options: {
+						maxZoom: 14 
+					}  
+				},
+				marker:{
+					address: "40 Sibley St, Detroit",
+				}
+			},
+			"autofit" );
+	    	
+	    }
+	});
+
+	$(window).load(function(){
+
+	});
+
+})(jQuery, document, window);
